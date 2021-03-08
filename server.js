d8ac95f34156c69fbddaf0b8c93d07f6954dc280
Sam Ayler <sayler8701@gmail.com>
2021-03-08 10:23:27 -0800

    setup server for production build

--- server.js
+++ server.js
@@ -12,4 +12,13 @@ app.use(express.json());
 // db connection
 require("./models/connection");
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+}
+
+//custom routes
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
 app.listen(PORT, () => console.log(`connected on http://localhost${PORT}`));
