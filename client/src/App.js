68f003215142305eea5fc5d183fa8840d0adf8bf
Sam Ayler <sayler8701@gmail.com>
2021-03-09 16:08:40 -0800

    setup userContext and local storge auth-token

--- client/src/App.js
+++ client/src/App.js
@@ -7,9 +7,25 @@ import M from "materialize-css/dist/js/materialize.min.js";
 import NavBar from "./Components/NavBar";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
+import Saved from "./Pages/Saved";
+import Search from "./Pages/Search";
+import UserContext from "./Context/userContext";
 
 function App() {
+	const [userData, setuserData] = useState({
+		user: undefined,
+		token: undefined,
+	});
+
+	const checkLoggin = () => {
+		let token = localStorage.getItem("auth-token");
+		if (token === null) {
+			localStorage.setItem("auth-token", "");
+		}
+	};
+
 	useEffect(() => {
+		checkLoggin();
 		let sidenav = document.querySelector("#slide-out");
 		M.Sidenav.init(sidenav, {});
 	}, []);
@@ -17,10 +33,14 @@ function App() {
 	return (
 		<div className="App">
 			<Router>
-				<Switch>
-					<Route path="/login" component={Login} />
-					<Route path="/register" component={Register} />
-				</Switch>
+				<UserContext.Provider value={{ userData, setuserData }}>
+					<Switch>
+						<Route path="/login" component={Login} />
+						<Route path="/register" component={Register} />
+						<Route path="/saved" component={Saved} />
+						<Route path="/" component={Search} />
+					</Switch>
+				</UserContext.Provider>
 			</Router>
 		</div>
 	);
