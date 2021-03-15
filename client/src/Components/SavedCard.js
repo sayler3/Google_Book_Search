c9573ba35a3188bc82d25ae06f3401b6bf557341
Sam Ayler <sayler8701@gmail.com>
2021-03-15 11:27:25 -0700

    getting saved books by user to the saved page and rendering cards with delete button functionaily working

---
+++ client/src/Components/SavedCard.js
@@ -0,0 +1,64 @@
+import React from "react";
+import axios from "axios";
+
+const SavedCard = (props) => {
+	const books = props.data;
+
+	const handleDelete = async (e) => {
+		try {
+			const id = e.target.getAttribute("data-value");
+			const authToken = localStorage.getItem("auth-token");
+			const deleteBook = await axios.delete(`/api/books/${id}`, {
+				headers: { "x-auth-token": authToken },
+			});
+			console.log(deleteBook);
+			console.log(id);
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	return (
+		<>
+			{books &&
+				books.map((book) => (
+					<div
+						style={{ boxShadow: "rgb(153 153 153) 0px 10px 10px" }}
+						key={book._id}
+						className="col s12 m7"
+					>
+						<h3 className="header">{book.title}</h3>
+						<div className="card horizontal">
+							<div className="card-image">
+								<img src={book.image} alt={book.title} />
+							</div>
+							<div className="card-stacked">
+								<div className="card-content">
+									<p>{book.description}</p>
+									<br />
+									<p>Authors: {book.authors}</p>
+								</div>
+								<div className="card-action">
+									<a target="blank" href={book.link}>
+										Preview
+									</a>
+									<button
+										onClick={handleDelete}
+										className="btn waves-effect waves-light"
+										type="button"
+										name="action"
+										data-value={book._id}
+									>
+										Delete
+										<i className="material-icons right">delete</i>
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				))}
+		</>
+	);
+};
+
+export default SavedCard;
--- client/src/Pages/Saved.js
+++ client/src/Pages/Saved.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
+import UserContext from "../Context/userContext";
+import { useHistory } from "react-router-dom";
+import SavedCard from "../Components/SavedCard";
+import axios from "axios";
 
 const Saved = () => {
+	const [books, setbooks] = useState(null);
+	const { userData } = useContext(UserContext);
+	const history = useHistory();
+	const [userID] = useState(userData.user?.id);
+	console.log(userID);
+
+	useEffect(() => {
+		if (!userData.user) history.push("/login");
+
+		console.log(userData.user);
+	}, [userData.user, history]);
+
+	useEffect(async () => {
+		const authToken = localStorage.getItem("auth-token");
+
+		const savedBooks = await axios.get(`/api/books/${userID}`, {
+			headers: { "x-auth-token": authToken },
+		});
+
+		console.log(savedBooks.data);
+
+		setbooks(savedBooks.data);
+	}, [userID]);
+
 	return (
 		<div className="container">
-			<h1>Hello from saved</h1>
+			<h1>Your saved books</h1>
+			{books && <SavedCard data={books} />}
 		</div>
 	);
 };
